0c6f861abc4ef008853ba4e1e5a3fddfdba0731b
Miguel Leonar <migsldev@gmail.com>
2024-03-01 10:34:49 +1100

    3 passing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    const userDetails = {
+        name: name,
+        email: email
+    }
+    const configData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userDetails)
+    };
+
+    return fetch("http://localhost:3000/users", configData)
+        .then(response => response.json())
+        .then(data => {
+            document.body.innerHTML += `<p>New ID: ${data.id}</p>`
+        })
+        .catch(error => {
+            console.error("Error", error);
+            document.body.innerHTML += `<p>Error Message: ${error.message}</p>`
+
+        });
+}
+submitData();
